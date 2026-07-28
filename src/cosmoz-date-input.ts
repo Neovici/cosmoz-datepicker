4b881ed571358a605f97e912dfae8f59254b53c5
Elias Landin <elias.landin@me.com>
2026-07-28 12:06:58 +0200

    feat(cosmoz-date-input): add cosmoz-date-input component

---
+++ src/cosmoz-date-input.ts
@@ -0,0 +1,166 @@
+import '@neovici/cosmoz-input';
+import '@neovici/cosmoz-tokens';
+import {
+	component,
+	css,
+	html,
+	useEffect,
+	useProperty,
+	useState,
+} from '@pionjs/pion';
+import { format } from 'date-fns';
+import { repeat } from 'lit-html/directives/repeat.js';
+import {
+	DateType,
+	isDateType,
+	parseDayInput,
+	parseMonthInput,
+	parseYearInput,
+} from './utils';
+
+type ValueChangedEvent = CustomEvent<{ value: string }>;
+
+interface Props extends HTMLElement {
+	value?: string;
+	locale?: string;
+	min?: string;
+	max?: string;
+}
+
+const options: Intl.DateTimeFormatOptions = {
+	year: 'numeric',
+	month: 'numeric',
+	day: 'numeric',
+};
+
+const CosmozDateInput = (host: Props) => {
+	const [value, setValue] = useProperty('value', '2026-04-21');
+	const { locale: _locale } = host;
+	const locale = _locale ?? navigator.language;
+	const date = value ? new Date(value) : undefined;
+	const parts = Intl.DateTimeFormat(locale, options).formatToParts(date);
+	const [inputState, setInputState] = useState({
+		year: date?.getFullYear()?.toString() ?? '',
+		month: date ? String(date?.getMonth() + 1) : '',
+		day: date?.getDate()?.toString() ?? '',
+	});
+
+	useEffect(() => {
+		if (Object.values(inputState).every((v) => v !== '')) {
+			setValue(
+				format(
+					new Date(
+						Number(inputState.year),
+						Number(inputState.month) - 1,
+						Number(inputState.day),
+					),
+					'yyyy-MM-dd',
+				),
+			);
+		}
+	}, [inputState]);
+
+	const onFocus = (e: InputEvent) => {
+		const target = e.currentTarget as HTMLInputElement;
+		const input = target.shadowRoot?.querySelector('input');
+		input?.select();
+	};
+
+	const onChange = (e: ValueChangedEvent, type: DateType) => {
+		const input = e.detail.value;
+		const val = Number(input);
+		setInputState((prev) => {
+			if (type === 'year') {
+				const year = val === 0 ? '' : parseYearInput(val);
+				return {
+					...prev,
+					year,
+				};
+			} else if (type === 'month') {
+				const month = val === 0 ? '' : parseMonthInput(val);
+				return {
+					...prev,
+					month,
+				};
+			} 
+				const day = val === 0 ? '' : parseDayInput(val, prev.month, prev.year);
+				return {
+					...prev,
+					day,
+				};
+			
+		});
+	};
+
+	return repeat(
+		parts,
+		(_part, i) => i,
+		({ value, type }) => {
+			if (isDateType(type)) {
+				return html`
+					<cosmoz-input
+						style="width: ${String(inputState[type]).length > 0
+							? String(inputState[type]).length
+							: 2}ch;"
+						type="number"
+						no-label-float
+						part=${type}
+						no-spinner
+						autocomplete="off"
+						placeholder="0000"
+						.value=${inputState[type]}
+						@focus=${onFocus}
+						@value-changed=${(e: ValueChangedEvent) => onChange(e, type)}
+					></cosmoz-input>
+				`;
+			}
+
+			if (type === 'literal') {
+				return html`<span>${value}</span>`;
+			}
+		},
+	);
+};
+
+const styles = css`
+	:host {
+		display: flex;
+		align-items: center;
+		gap: calc(var(--cz-spacing) * 1.5);
+		position: relative;
+		font-size: var(--cz-text-base);
+		line-height: var(--cz-text-base-line-height);
+		font-family: var(--cz-font-body);
+		padding-block: calc(var(--cz-spacing) * 2);
+		padding-inline: calc(var(--cz-spacing) * 3);
+		border-radius: var(--cz-radius-md);
+		box-shadow: 0 0 0 1px var(--cz-color-border-primary);
+	}
+
+	cosmoz-input {
+		margin: 0;
+	}
+
+	cosmoz-input::part(input) {
+		padding: 0;
+	}
+
+	cosmoz-input::part(wrap) {
+		box-shadow: none;
+		border-radius: 0;
+	}
+`;
+
+customElements.define(
+	'cosmoz-date-input',
+	component(CosmozDateInput, {
+		observedAttributes: ['min', 'max'],
+		styleSheets: [styles],
+		shadowRootInit: {
+			delegatesFocus: true,
+			mode: 'open',
+		},
+	}),
+);
+
+export { CosmozDateInput as CosmozDatepicker };
