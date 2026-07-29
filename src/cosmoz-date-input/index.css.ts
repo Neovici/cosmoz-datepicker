675ea4c9862e28667b129d7b6ae02c4c1275c5d8
Elias Landin <elias.landin@me.com>
2026-07-29 14:01:08 +0200

    style: update styling

--- src/cosmoz-date-input/index.css.ts
+++ src/cosmoz-date-input/index.css.ts
@@ -4,13 +4,12 @@ export const styles = css`
 	:host {
 		display: flex;
 		align-items: center;
-		gap: var(--cz-spacing);
+		gap: calc(var(--cz-spacing) * 0.5);
 		position: relative;
 		font-size: var(--cz-text-base);
 		line-height: var(--cz-text-base-line-height);
 		font-family: var(--cz-font-body);
-		padding-block: calc(var(--cz-spacing) * 2);
-		padding-inline: calc(var(--cz-spacing) * 3);
+		padding: calc(var(--cz-spacing) * 2);
 		border-radius: var(--cz-radius-md);
 		box-shadow: 0 0 0 1px var(--cz-color-border-primary);
 		width: min-content;
@@ -22,8 +21,7 @@ export const styles = css`
 
 	cosmoz-input {
 		margin: 0;
-		padding: 0 4px;
-		box-sizing: content-box;
+		padding: 0 calc(var(--cz-spacing) * 1);
 	}
 
 	cosmoz-input:focus-within {
