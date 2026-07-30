1a7b3755b4e5e44730d73016e5ea0066c9b69afa
Elias Landin <elias.landin@me.com>
2026-07-30 10:37:47 +0200

    feat: add support for multiple calendars and add rendering and styling

--- src/calendar/calendar.ts
+++ src/calendar/calendar.ts
@@ -1,46 +1,120 @@
 import { normalize } from '@neovici/cosmoz-tokens/normalize';
 import { ensureDate } from '@neovici/cosmoz-utils/date';
-import { component, html, useMemo, useProperty } from '@pionjs/pion';
+import { component, css, html, useMemo, useProperty } from '@pionjs/pion';
 import { repeat } from 'lit-html/directives/repeat.js';
 import { getMonthsDateCellMatrix } from './utils';
 
 type CalendarProps = HTMLElement & {
 	value?: string;
 	locale?: string;
+	numberOfMonths?: string;
 };
 
 const CosmozCalendar = (host: CalendarProps) => {
-	const { locale: _locale } = host;
+	const { locale: _locale, numberOfMonths: _numberOfMonths } = host;
 	const locale = useMemo(() => _locale ?? navigator.language, [_locale]);
+	const numberOfMonths = Number(_numberOfMonths ?? 2);
 	const [value] = useProperty<string>('value');
 	const date = useMemo(() => ensureDate(value) ?? new Date(), [value]);
 
-	const dateCells = useMemo(
-		() => getMonthsDateCellMatrix(date, locale),
-		[date, locale],
-	);
+	const monthMatrices = useMemo(() => {
+		const matrices = [];
+		for (let i = 0; i < numberOfMonths; i++) {
+			const relDate = new Date(
+				date.getFullYear(),
+				date.getMonth() + i,
+				date.getDate(),
+			);
 
-	return html`<table>
+			matrices.push(getMonthsDateCellMatrix(relDate, locale));
+		}
+		return matrices;
+	}, [date, locale]);
+
+	return html`<div class="calendar-wrapper">
 		${repeat(
-			dateCells,
-			(_, i) => i,
-			(week) => html`
-				<tr>
+			monthMatrices,
+			(_, i) => `cal-${i}`,
+			(month) => html`
+				<table class="${numberOfMonths === 1 ? 'is-single-month' : ''}">
 					${repeat(
-						week,
-						(day) => day.date,
-						(day) => html` <td>${day.day}</td> `,
+						month,
+						(_, i) => `month-${i}`,
+						(week) => html`
+							<tr>
+								${repeat(
+									week,
+									(day) => day.date,
+									(day) => html`
+										<td>
+											<div
+												class="
+													date-cell
+													${day.isToday && day.isCurrentMonth ? 'today-cell' : ''}
+													${!day.isCurrentMonth ? 'other-month-cell' : ''}
+												"
+												tabindex=${day.isToday ? '0' : '-1'}
+											>
+												${day.day}
+											</div>
+										</td>
+									`,
+								)}
+							</tr>
+						`,
 					)}
-				</tr>
+				</table>
 			`,
 		)}
-	</table>`;
+	</div> `;
 };
+
+const styles = css`
+	:host {
+		--cell-size: calc(var(--cz-spacing) * 10);
+	}
+
+	.calendar-wrapper {
+		display: flex;
+	}
+
+	table {
+		padding: calc(var(--cz-spacing) * 6);
+	}
+
+	tr {
+		margin-bottom: var(--cz-spacing);
+	}
+
+	.date-cell {
+		width: var(--cell-size);
+		height: var(--cell-size);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: var(--cz-radius-full);
+		user-select: none;
+		cursor: pointer;
+	}
+
+	.today-cell {
+		background: var(--cz-color-bg-brand-solid);
+		color: var(--cz-color-text-on-brand);
+	}
+
+	.other-month-cell {
+		color: transparent;
+	}
+
+	.is-single-month .other-month-cell {
+		color: var(--cz-color-text-placeholder-subtle);
+	}
+`;
 
 customElements.define(
 	'cosmoz-calendar',
 	component(CosmozCalendar, {
-		observedAttributes: ['locale'],
-		styleSheets: [normalize],
+		observedAttributes: ['locale', 'number-of-months'],
+		styleSheets: [normalize, styles],
 	}),
 );
