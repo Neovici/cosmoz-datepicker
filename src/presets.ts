d5d6ee05376dbfe5e7cae123db9ede9fb359d03a
Elias Landin <elias.landin@me.com>
2026-08-04 15:45:29 +0200

    feat: add more presets

--- src/presets.ts
+++ src/presets.ts
@@ -1,4 +1,14 @@
-import { addDays, format, subDays } from 'date-fns';
+import {
+	addDays,
+	endOfMonth,
+	endOfYear,
+	format,
+	startOfMonth,
+	startOfYear,
+	subDays,
+	subMonths,
+	subWeeks,
+} from 'date-fns';
 import { t } from 'i18next';
 import { startOfWeek } from './utils';
 
@@ -24,5 +34,27 @@ export const getRangePresets = (locale: string): RangePreset[] => [
 		start: () => format(startOfWeek(new Date(), locale), 'yyyy-MM-dd'),
 		end: () =>
 			format(addDays(startOfWeek(new Date(), locale), 6), 'yyyy-MM-dd'),
+	},
+	{
+		label: t('Last week'),
+		start: () =>
+			format(subWeeks(startOfWeek(new Date(), locale), 1), 'yyyy-MM-dd'),
+		end: () =>
+			format(subDays(startOfWeek(new Date(), locale), 1), 'yyyy-MM-dd'),
+	},
+	{
+		label: t('This month'),
+		start: () => format(startOfMonth(new Date()), 'yyyy-MM-dd'),
+		end: () => format(endOfMonth(new Date()), 'yyyy-MM-dd'),
+	},
+	{
+		label: t('Last month'),
+		start: () => format(subMonths(startOfMonth(new Date()), 1), 'yyyy-MM-dd'),
+		end: () => format(subMonths(endOfMonth(new Date()), 1), 'yyyy-MM-dd'),
+	},
+	{
+		label: t('This year'),
+		start: () => format(startOfYear(new Date()), 'yyyy-MM-dd'),
+		end: () => format(endOfYear(new Date()), 'yyyy-MM-dd'),
 	},
 ];
