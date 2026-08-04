c12466218efa538d7c328b64c520c2c559639b2b
Elias Landin <elias.landin@me.com>
2026-08-04 14:37:02 +0200

    feat: integrate cosmoz-dropdown

--- src/cosmoz-datepicker.ts
+++ src/cosmoz-datepicker.ts
@@ -1,4 +1,5 @@
 import '@neovici/cosmoz-button';
+import '@neovici/cosmoz-dropdown';
 import { normalize } from '@neovici/cosmoz-tokens/normalize';
 import { useProperty } from '@neovici/cosmoz-utils/hooks/use-property';
 import { component, css, html, lift } from '@pionjs/pion';
@@ -10,58 +11,71 @@ interface Props {
 	value?: string;
 	min?: string;
 	max?: string;
+	disabled?: boolean;
 }
 
 const CosmozDatepicker = (host: Props) => {
-	const { min, max } = host;
+	const { min, max, disabled } = host;
 	const [start, setStart] = useProperty('start');
 	const [end, setEnd] = useProperty('end');
 
 	return html`
-		<div class="contents">
-			<div class="range-presets">
-				<cosmoz-button variant="tertiary" size="sm" full-width
-					>Today</cosmoz-button
-				>
-			</div>
-			<div class="main">
-				<cosmoz-calendar
-					locale="sv-SE"
-					number-of-months="2"
-					.min=${ifDefined(min)}
-					.max=${ifDefined(max)}
-					.start=${start}
-					.end=${end}
-					@start-changed=${lift(setStart)}
-					@end-changed=${lift(setEnd)}
-				></cosmoz-calendar>
+		<cosmoz-dropdown-next ?disabled=${ifDefined(disabled)}>
+			<cosmoz-button slot="button" variant="secondary">Open</cosmoz-button>
 
-				<footer>
-					<div class="footer-left">
-						<cosmoz-date-input
-							locale="sv-SE"
-							.value=${start}
-							@value-changed=${lift(setStart)}
-						></cosmoz-date-input>
-						<span>–</span>
-						<cosmoz-date-input
-							locale="sv-SE"
-							.value=${end}
-							@value-changed=${lift(setEnd)}
-						></cosmoz-date-input>
-					</div>
-					<div>
-						<cosmoz-button>OK</cosmoz-button>
-					</div>
-				</footer>
+			<div class="content">
+				<div class="range-presets">
+					<cosmoz-button variant="tertiary" full-width>Today</cosmoz-button>
+				</div>
+				<div class="main">
+					<cosmoz-calendar
+						locale="sv-SE"
+						number-of-months="2"
+						.min=${ifDefined(min)}
+						.max=${ifDefined(max)}
+						.start=${start}
+						.end=${end}
+						@start-changed=${lift(setStart)}
+						@end-changed=${lift(setEnd)}
+					></cosmoz-calendar>
+
+					<footer>
+						<div class="footer-left">
+							<cosmoz-date-input
+								locale="sv-SE"
+								.value=${start}
+								@value-changed=${lift(setStart)}
+							></cosmoz-date-input>
+							<span>–</span>
+							<cosmoz-date-input
+								locale="sv-SE"
+								.value=${end}
+								@value-changed=${lift(setEnd)}
+							></cosmoz-date-input>
+						</div>
+						<div>
+							<cosmoz-button
+								@click=${(e: MouseEvent) =>
+									e.target?.dispatchEvent(
+										new Event('select', { bubbles: true }),
+									)}
+								>OK</cosmoz-button
+							>
+						</div>
+					</footer>
+				</div>
 			</div>
-		</div>
+		</cosmoz-dropdown-next>
 	`;
 };
 
 const styles = css`
-	.contents {
+	.content {
 		display: flex;
+		background: var(--cz-color-bg-primary);
+		border: 1px solid var(--cz-color-border-secondary);
+		border-radius: var(--cz-radius-2xl);
+		box-shadow: var(--cz-shadow-xl);
 	}
 
 	.range-presets {
@@ -93,7 +107,7 @@ const styles = css`
 customElements.define(
 	'cosmoz-datepicker',
 	component(CosmozDatepicker, {
-		observedAttributes: ['min', 'max'],
+		observedAttributes: ['min', 'max', 'disabled'],
 		styleSheets: [normalize, styles],
 		shadowRootInit: { delegatesFocus: true, mode: 'open' },
 	}),
