a8415d738c19a0f51bf8d36f66cad08ed993e02f
Elias Landin <elias.landin@me.com>
2026-08-05 09:08:33 +0200

    feat: add useMediaMatch hook

---
+++ src/use-media-match.ts
@@ -0,0 +1,16 @@
+import { useEffect, useMemo, useState } from '@pionjs/pion';
+
+export const useMediaMatch = (query: string) => {
+	const match = useMemo(() => matchMedia(query), [query]);
+	const [isMatch, setIsMatch] = useState(match.matches);
+
+	useEffect(() => {
+		const onMatch = (e: MediaQueryListEvent) =>
+			setIsMatch((prev) => (prev !== e.matches ? e.matches : prev));
+
+		match.addEventListener('change', onMatch);
+		return () => match.removeEventListener('change', onMatch);
+	});
+
+	return isMatch;
+};
