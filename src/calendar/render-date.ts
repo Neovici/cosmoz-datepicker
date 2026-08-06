cfbe18238e429b26393651b649804f78863e0a0e
Elias Landin <elias.landin@me.com>
2026-08-06 16:09:48 +0200

    style: improve range background styling

--- src/calendar/render-date.ts
+++ src/calendar/render-date.ts
@@ -43,46 +43,53 @@ export const renderDate = ({
 	const date = new Date(day.iso);
 
 	return html`
-		<td
-			class="${isInRange(date, startDate, endDate, focusedDate)
-				? 'in-range'
-				: ''}"
-		>
+		<td>
 			<div
-				class=${classMap({
-					'date-cell': true,
-					'selected-cell': isSelected(date, startDate, endDate),
-					'focused-highlighted-cell':
-						!endDate && !!startDate && isSameDay(date, focusedDate),
-					'today-cell': day.isToday && day.isCurrentMonth,
-					'other-month-cell': !day.isCurrentMonth,
-					'hidden-cell': !day.isCurrentMonth && numberOfMonths > 1,
-				})}
-				role="button"
-				tabindex=${isSameDay(date, focusedDate) ? '0' : '-1'}
-				aria-label=${date.toLocaleString(locale, {
-					weekday: 'long',
-					year: 'numeric',
-					month: 'long',
-					day: 'numeric',
-				})}
-				aria-disabled=${ifDefined(
-					ifDisabled(date, day.isCurrentMonth, minDate, maxDate),
-				)}
-				data-date=${ifDefined(
-					day.isCurrentMonth ? format(date, 'yyyy-MM-dd') : undefined,
-				)}
-				data-disabled=${ifDefined(
-					ifDisabled(date, day.isCurrentMonth, minDate, maxDate),
-				)}
-				data-start=${ifDefined(ifStart(day, startDate))}
-				data-end=${ifDefined(ifEnd(day, endDate))}
-				@pointerdown=${onPointerDown}
-				@click=${onClick}
-				@pointerenter=${onPointerEnter}
-				@focus=${onFocus}
+				class="date-cell-wrapper ${isInRange(
+					date,
+					startDate,
+					endDate,
+					focusedDate,
+				)
+					? 'in-range'
+					: ''}"
 			>
-				${day.day}
+				<div
+					class=${classMap({
+						'date-cell': true,
+						'selected-cell': isSelected(date, startDate, endDate),
+						'focused-highlighted-cell':
+							!endDate && !!startDate && isSameDay(date, focusedDate),
+						'today-cell': day.isToday && day.isCurrentMonth,
+						'other-month-cell': !day.isCurrentMonth,
+						'hidden-cell': !day.isCurrentMonth && numberOfMonths > 1,
+					})}
+					role="button"
+					tabindex=${isSameDay(date, focusedDate) ? '0' : '-1'}
+					aria-label=${date.toLocaleString(locale, {
+						weekday: 'long',
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+					})}
+					aria-disabled=${ifDefined(
+						ifDisabled(date, day.isCurrentMonth, minDate, maxDate),
+					)}
+					data-date=${ifDefined(
+						day.isCurrentMonth ? format(date, 'yyyy-MM-dd') : undefined,
+					)}
+					data-disabled=${ifDefined(
+						ifDisabled(date, day.isCurrentMonth, minDate, maxDate),
+					)}
+					data-start=${ifDefined(ifStart(day, startDate))}
+					data-end=${ifDefined(ifEnd(day, endDate))}
+					@pointerdown=${onPointerDown}
+					@click=${onClick}
+					@pointerenter=${onPointerEnter}
+					@focus=${onFocus}
+				>
+					${day.day}
+				</div>
 			</div>
 		</td>
 	`;
