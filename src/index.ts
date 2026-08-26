75b5a285d5331b7de831d4a5a70a6855cef36993
Elias Landin <elias.landin@me.com>
2026-08-26 14:53:31 +0200

    fix: export datepicker types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export { CosmozCalendar } from './calendar/calendar';
 export { CosmozDatepicker } from './cosmoz-datepicker';
 export { CosmozDateInput } from './date-input/date-input';
+export type { RangePreset } from './presets';
+export type {
+	DateRange,
+	DatepickerMode,
+	DatepickerValue,
+} from './use-datepicker';
